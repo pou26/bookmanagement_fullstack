b539ef7dd1af5ab4e082dedb5ada890080a9b590
Sushant Bhaiswar <96516472+SushantBhaiswar@users.noreply.github.com>
2023-01-26 22:49:51 +0530

    production done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require("cors")
 app.use(express.json());
+require("dotenv").config()
 
 mongoose.connect("mongodb+srv://Sushant_Bhaiswar_30:WBYUu1bCYmxmZUmg@cluster0.jui41on.mongodb.net/group17Database", {
     useNewUrlParser: true
@@ -14,6 +15,25 @@ mongoose.connect("mongodb+srv://Sushant_Bhaiswar_30:WBYUu1bCYmxmZUmg@cluster0.ju
 app.use(cors())
 app.use('/', route)
 
-app.listen(3001, function () {
+
+//vercel 
+
+if (process.env.NODE_ENV == 'production') {
+    const path = require('path')
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", function (_, res) {
+        res.sendFile(
+            path.join(__dirname, "../client/build/index.html"),
+            function (err) {
+                if (err) {
+                    res.status(500).send(err)
+                }
+            }
+        )
+    })
+}
+
+app.listen(process.env.PORT || 3001, function () {
     console.log('Express app running on port ' + 3001)
 });
